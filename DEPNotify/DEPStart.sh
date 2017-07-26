0a5c373cb7921d9329df28c9623096ea5fc2089f
Jon Yergatian <jon@macfanatic.ca>
2017-07-26 16:39:49 -0700

    reworked variables

--- DEPNotify/DEPStart.sh
+++ DEPNotify/DEPStart.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
 #################### Variables ####################
-# Set $4 to "Support Link (Default: https://www.apple.com/business/dep/"
-support_link=
-# Set $5 to "Image Path (Default: None)"
-image_path=
-# Set $6 to "Welcome Message (Default: Welcome to your new Mac! \n \n We're running some tasks in the background to complete the setup process.)"
-welcome_message=
-# Set $7 to "Window Title (Default: Hello!)"
-window_title=
-# Set $8 to "Intro Status (Default: Beginning automated setup)"
-intro_status=
+# Set $4 to "DEPNotify Path (Default: /Applications/DEPNotify.app)"
+depnotify_path="/Library/Macfanatic/bin/DEPNotify.app"
+# Set $5 to "Support Link (Default: https://www.apple.com/business/dep/)"
+support_link=""
+# Set $6 to "Image Path (Default: None)"
+image_path=""
+# Set $7 to "Welcome Message (Default: Welcome to your new Mac!)"
+welcome_message=""
+# Set $8 to "Window Title (Default: Hello!)"
+window_title=""
+# Set $9 to "Intro Status (Default: Beginning automated setup)"
+intro_status=""
 ################## Do Not Modify ##################
 
 DATE=$(date "+%Y%m%d-%H%M%S")
 
 # Incase we don't specify anything
-## Support Link
+## DEPNotify Path
 if [[ $4 ]]; then
-  support_link=$4
+  depnotify_path=$4
+elif [[ -z $depnotify_path ]]; then
+  depnotify_path="/Applications/DEPNotify.app"
+fi
+## Support Link
+if [[ $5 ]]; then
+  support_link=$5
 elif [[ -z $support_link ]]; then
   support_link="https://www.apple.com/business/dep/"
 fi
 ## Image Path
-if [[ $5 ]]; then
-  image_path=$5
+if [[ $6 ]]; then
+  image_path=$6
 fi
 ## Welcome Message
-if [[ $6 ]]; then
-  welcome_message=$6
+if [[ $7 ]]; then
+  welcome_message=$7
 elif [[ -z $welcome_message ]]; then
-  welcome_message="Welcome to your new Mac! \n \n We're running some tasks in the background to complete the setup process."
+  welcome_message="Welcome to your new Mac!"
 fi
 ## Window Title
-if [[ $7 ]]; then
-  window_title=$7
+if [[ $8 ]]; then
+  window_title=$8
 elif [[ -z $window_title ]]; then
   window_title="Hello!"
 fi
 ## Intro Status
-if [[ $8 ]]; then
-  intro_status=$8
+if [[ $9 ]]; then
+  intro_status=$9
 elif [[ -z $intro_status ]]; then
   intro_status="Beginning automated setup"
 fi
@@ -61,7 +69,7 @@ echo "Command: WindowTitle: $window_title" >> /var/tmp/depnotify.log
 # Make sure it's readable
 chmod 644 /var/tmp/depnotify.log
 # Start the process
-open /Library/Macfanatic/bin/DEPNotify.app
+open "$depnotify_path"
 # Announce new begginings
 echo "Status: $intro_status" >> /var/tmp/depnotify.log
 

ccf70e906682dd96ba3292549431f1ca1b4cf9e4
Jon Yergatian <jon@macfanatic.ca>
2017-09-03 11:40:03 -0700

    added support for sepcifying protocol

--- Printers/Printer_Install.sh
+++ Printers/Printer_Install.sh
@@ -5,8 +5,15 @@
 ## $6 = Location
 ## $7 = IP Address
 ## $8 = Driver Location
+## $9 = Protocol (Default: ipp://)
 
-/usr/sbin/lpadmin -p "$4" -D "$5" -L "$6" -E -o printer-is-shared=false -v lpd://$7 -P "$8"
+if [[ $9 ]]; then
+  protocol="$9"
+else
+  protocol="ipp://"
+fi
+
+/usr/sbin/lpadmin -p "$4" -D "$5" -L "$6" -E -o printer-is-shared=false -v $protocol$7 -P "$8"
 if [ "$?" == 0 ]; then
     echo "Printer configured correctly"
     exit 0
